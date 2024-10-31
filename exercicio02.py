daddb45ac750ead2b3af42d812b0922fcd04d7dc
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2024-10-31 14:49:46 -0300

    exercicio 2 concluido

--- exercicio02.py
+++ exercicio02.py
@@ -1,11 +1,11 @@
-tupla1 = (5,3,2)
-tupla2 = (3,2,3)
+tupla1 = (5,3,'a',3)
+tupla2 = (3,'a',5,3)
 diferente = False
 if(len(tupla1) != len(tupla2)):
     diferente = True
 else:
     for item in tupla1:
-        if(item not in tupla2):
+        if(tupla1.count(item) != tupla2.count(item)):
             diferente = True
             break
 print("Diferente" if diferente else "Igual")
