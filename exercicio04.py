d6f5a5c2011590ff807bb37a8c259dcfccdfa8df
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2024-11-01 10:03:18 -0300

    exercicio04.py

--- exercicio04.py
+++ exercicio04.py
@@ -1,2 +1,24 @@
-dicionario1 = {'times': ["Flamengo", "Vasco", "Botafogo", "Fluminense"]}
-print('times' in dicionario1)
+def verificarSubconjunto(dicionario1, dicionario2):
+    subconjunto = True
+    if(len(dicionario1) > len(dicionario2)):
+        for c in dicionario2.keys():
+            if(dicionario2.get(c) != dicionario1.get(c)):
+                subconjunto = False
+                break
+        print("Segundo dicionário é subconjunto do primeiro" if subconjunto else "Segundo dicionário não é subconjunto do primeiro")
+    elif (len(dicionario1) < len(dicionario2)):
+        for c in dicionario1.keys():
+            if(dicionario1.get(c) != dicionario2.get(c)):
+                subconjunto = False
+                break
+        print("Primeiro dicionário é subconjunto do segundo" if subconjunto else "Primeiro dicionário não é subconjunto do segundo")
+    else:
+        for c in dicionario1.keys():
+            if(dicionario1.get(c) != dicionario2.get(c)):
+                subconjunto = False
+                break
+        print("São iguais" if subconjunto else "São diferentes")
+
+idades = {'Alice': 22, 'Bob': 17, 'Carol': 19, 'David': 16, "José": 4, "Joana": 7}
+adultos = {'Alice': 22, 'Carol': 19, "José": 4}
+verificarSubconjunto(adultos, idades)
