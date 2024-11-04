08b58ce5a3252ab6a092182b960727d0e3b18483
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2024-11-04 12:25:34 -0300

    exercicio10.py

--- exercicio10.py
+++ exercicio10.py
@@ -1,23 +1,37 @@
 turma = [
-            {"nome": "Ana", "nota": 5},
+            {"nome": "Ana", "nota": 10},
             {"nome": "Julia", "nota": 6},
-            {"nome": "Joana", "nota": 7},
+            {"nome": "Joana", "nota": 2},
             {"nome": "Maria", "nota": 9},
             {"nome": "Sabrina", "nota": 10},
         ]
+        
+def ordenarPorNotaMaiorAMenor(turma):
+    continua = True
+    i = 0
+    fim = len(turma)-1
+    contadorMenorQueZero = 0
+    while(continua):
+        if i < fim:
+            comparacao = turma[i].get('nota') - turma[i+1].get('nota')
+            if comparacao < 0:
+                contadorMenorQueZero+=1
+                temp = turma[i+1]
+                turma[i+1] = turma[i]
+                turma[i] = temp
+        elif i == fim:
+            if contadorMenorQueZero == 0:
+                continua = False
+            else:
+                i = -1
+            contadorMenorQueZero = 0
+        i+=1
 
-for i in range(len(turma)):
-    if(i < (len(turma) - 1)):   
-        temp = turma[i]
-        diferenca = turma[i].get('nota') < turma[i+1].get('nota')
-        if turma[i].get('nota') < turma[i+1].get('nota'):
-            turma[i] = turma[i+1]
-            turma[i+1] = temp
-        elif turma[i].get('nota') > turma[i+1].get('nota'):
-            turma[i] = turma[i+1]
-            turma[i+1] = temp
-        else:
-            continue
+def exibirTurma(turma):
+    for i in turma:
+        print(i)
 
-for i in turma:
-    print(i)
+exibirTurma(turma)
+ordenarPorNotaMaiorAMenor(turma)
+print()
+exibirTurma(turma)
